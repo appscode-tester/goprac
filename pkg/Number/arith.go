a7d3a5622fab6033a4770182f19ce1238024aba3
Md. Ashiquzzaman <ashiquzzaman33@gmail.com>
2017-04-10 17:53:26 +0600

    initial commit

---
+++ pkg/Number/arith.go
@@ -0,0 +1,84 @@
+package multiplication
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	Int string = "int"
+	Doub string = "double"
+)
+type Number struct {
+	t string
+	v interface{}
+}
+
+func NewNumber(n interface{}) (Number, error) {
+	switch n.(type) {
+	case int:
+		return Number{Int, int64(n.(int))}, nil
+	case int8:
+		return Number{Int, int64(n.(int8))}, nil
+	case int16:
+		return Number{Int, int64(n.(int16))}, nil
+	case int32:
+		return Number{Int, int64(n.(int32))}, nil
+	case int64:
+		return Number{Int, n}, nil
+	case float32:
+		return Number{Doub, float64(n.(float32))}, nil
+	case float64:
+		return Number{Doub, n}, nil
+	default:
+		return  Number{}, errors.New("type doesn't currently supported")
+	}
+}
+
+func (n *Number) Mul(num Number) (Number, error) {
+	if n.t != num.t {
+		return Number{}, errors.New("type mismatch")
+	}
+	switch n.t {
+	case Int:
+		f := n.v.(int64)
+		s := num.v.(int64)
+		return Number{
+			t: Int,
+			v: f*s,
+		}, nil
+	case Doub:
+		f := n.v.(float64)
+		s := num.v.(float64)
+		return Number{
+			t: Doub,
+			v: f*s,
+		}, nil
+	default:
+		return Number{}, fmt.Errorf("type %v doesn't supported", n.t)
+	}
+}
+
+func (n *Number) Add(num Number) (Number, error) {
+	if n.t != num.t {
+		return Number{}, errors.New("type mismatch")
+	}
+	switch n.t {
+	case Int:
+		f := n.v.(int64)
+		s := num.v.(int64)
+		return Number{
+			t: Int,
+			v: f+s,
+		}, nil
+	case Doub:
+		f := n.v.(float64)
+		s := num.v.(float64)
+		return Number{
+			t: Doub,
+			v: f+s,
+		}, nil
+	default:
+		return Number{}, fmt.Errorf("type %v doesn't supported", n.t)
+	}
+}
---
+++ pkg/Number/arith_test.go
@@ -0,0 +1,29 @@
+package multiplication
+
+import (
+	"testing"
+)
+
+func TestNumberMul(t *testing.T) {
+	var a, b int = 10, 20
+	f1, _ := NewNumber(a)
+	f2, _ := NewNumber(b)
+	org, _ := f1.Mul(f2)
+	exp , _ := NewNumber(a*b)
+	if exp != org {
+		t.Error("expeted: ", exp, " but found: ", org)
+	}
+	return
+}
+
+func TestNumberAdd(t *testing.T) {
+	var a, b int = 10, 20
+	f1, _ := NewNumber(a)
+	f2, _ := NewNumber(b)
+	org, _ := f1.Add(f2)
+	exp , _ := NewNumber(a+b)
+	if exp != org {
+		t.Error("expeted: ", exp, " but found: ", org)
+	}
+	return
+}
